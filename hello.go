2d9e7cd2fbdde139992af8669b4c924fe6b02107
Eric Zeng <ericzeng2@gmail.com>
2015-02-14 23:48:30 -0800

    Host Auctora pitch slides on the application.
    The slides we used for our pitches are a website created using
    simplePrez (http://github.com/eric-zeng/simplePrez). Move the
    presentation files to this project, and modify hello.go to serve
    the content. The slides are available at
    tidy-nomad-842.appspot.com/slides
    
    Hopefully this is a good example for how to write HTTP handlers and
    serve files from the backend.

--- hello.go
+++ hello.go
@@ -1,14 +1,42 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
+
+	// Handlers for Auctora pitch slides.
+	http.HandleFunc("/slides", slidesIndexHandler)
+	http.HandleFunc("/slides/", slidesHandler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+	fmt.Fprint(w, "Hello, world!")
+}
+
+// Serve the root page for the Auctora slides.
+func slidesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	slidesHtml, err := ioutil.ReadFile("slides/auctora.html")
+	if err != nil {
+		fmt.Fprintf(w, "Couldn't read slides/auctora.html")
+		return
+	}
+	w.Write(slidesHtml)
+}
+
+// Serve files in the slides/ directory.
+func slidesHandler(w http.ResponseWriter, r *http.Request) {
+	requestedFile := strings.TrimPrefix(r.URL.Path, "/") // Remove the leading slash
+
+	file, err := ioutil.ReadFile(requestedFile)
+	if err != nil {
+		fmt.Fprintf(w, "Couldn't read %s", requestedFile)
+		return
+	}
+	w.Write(file)
 }
