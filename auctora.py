7d870011e388bfe9260e601817799fa086780679
Eric Zeng <ericzeng2@gmail.com>
2015-03-01 17:23:46 -0800

    Move slides files around, and fix static file handling
    Slides were being a problem because of relative paths and programatic
    handlers and such. I moved the slides html/js/css into the same
    directories as the rest of the site. Also use the static handlers
    as much as possible.

--- auctora.py
+++ auctora.py
@@ -9,48 +9,35 @@ import webapp2
 import jinja2
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
+	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+	extensions=['jinja2.ext.autoescape'],
+	autoescape=True)
 
 # handler for URL with no path (just tidy-nomad-842.appspot.com)
 # shows the Auctora login page
 class LandingHandler(webapp2.RequestHandler):
-    def get(self):
-    	requestedFile = self.request.url[1:]
-    	logging.info("" + requestedFile)
-    	template = JINJA_ENVIRONMENT.get_template('html/LoginPage.html')
-        self.response.write(template.render())
-
-# serve any file referenced with an explicit path in the URL
-# class FileHandler(webapp2.RequestHandler):
-# 	def get(self):
-# 		requestedFile = self.request.url[1:]
-# 		logging.debug("" + requestedFile)
-# 		template = JINJA_ENVIRONMENT.get_template(requestedFile)
-# 		self.response.write(template.render())
+	def get(self):
+		requestedFile = self.request.url[1:]
+		logging.info("" + requestedFile)
+		template = JINJA_ENVIRONMENT.get_template('html/LoginPage.html')
+		self.response.write(template.render())
 
 # serve the root page for the Auctora slides
 class SlidesLandingHandler(webapp2.RequestHandler):
 	def get(self):
-		template = JINJA_ENVIRONMENT.get_template('slides/auctora.html')
+		requestedFile = self.request.url[1:]
+		logging.info("" + requestedFile)
+		template = JINJA_ENVIRONMENT.get_template('html/slides.html')
 		self.response.write(template.render())
 
 application = webapp2.WSGIApplication([
-    # root path handler
-    ('/', LandingHandler),
+	# root path handler
+	('/', LandingHandler),
 
-    # auctora pitch slides handlers
-    ('/slides', SlidesLandingHandler),
+	# auctora pitch slides handlers
+	('/slides', SlidesLandingHandler),
 
-    # LinkedIn auth handler
-    # ('/auth/linkedIn', authHandler),
-
-    # static file handlers
-    ('/css/', FileHandler),
-    ('/js/', FileHandler),
-    ('/fonts/', FileHandler),
-    ('/images/', FileHandler),
-    ('/html/', FileHandler),
+	# LinkedIn auth handler
+	# ('/auth/linkedIn', authHandler),
 
 ], debug=True)
