7cfb87095982e447ca964bb7b9b8c130aa0fb7d9
dju90 <dju90@uw.edu>
2015-03-04 12:19:45 -0800

    updated to redirect to chip modified html pages

--- auctora.py
+++ auctora.py
@@ -21,7 +21,7 @@ class LandingHandler(webapp2.RequestHandler):
 	def get(self):
 		requestedFile = self.request.url[1:]
 		logging.info('' + requestedFile)
-		template = JINJA_ENVIRONMENT.get_template('html/LoginPage.html')
+		template = JINJA_ENVIRONMENT.get_template('html/LoginPage-chip.html')
 		self.response.write(template.render())
 
 # serve the root page for the Auctora slides
@@ -99,12 +99,14 @@ class LinkedInAuthHandler(webapp2.RequestHandler):
 
 class QuestionsHandler(webapp2.RequestHandler):
 	def get(self):
-		template = JINJA_ENVIRONMENT.get_template('html/questions.html')
+		template = JINJA_ENVIRONMENT.get_template('html/questions-chip.html')
 		self.response.write(template.render())
 
 class QuestionsFormHandler(webapp2.RequestHandler):
 	def post(self):
 		logging.info(self.request.get('content'))
+		template = JINJA_ENVIRONMENT.get_template('html/companies-chip.html')
+		self.response.write(template.render())
 
 application = webapp2.WSGIApplication([
 	# Home page handler
