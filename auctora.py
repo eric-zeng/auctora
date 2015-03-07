fa30f1284ef0635f155cf78195fcedd3d6290dd0
dju90 <dju90@uw.edu>
2015-03-07 14:40:00 -0800

    Revert "changed names of chip's htmls to normal html names and included redirect to them as well (removed old htmls)"
    This reverts commit f275be87ca0f65918d9202027e0bdd1cf5abf519.

--- auctora.py
+++ auctora.py
@@ -32,7 +32,7 @@ class LandingHandler(webapp2.RequestHandler):
 	def get(self):
 		requestedFile = self.request.url[1:]
 		logging.info('' + requestedFile)
-		template = JINJA_ENVIRONMENT.get_template('html/LoginPage.html')
+		template = JINJA_ENVIRONMENT.get_template('html/LoginPage-chip.html')
 		self.response.write(template.render())
 
 # serve the root page for the Auctora slides
@@ -140,12 +140,12 @@ class LinkedInAuthHandler(webapp2.RequestHandler):
 
 class QuestionsHandler(webapp2.RequestHandler):
 	def get(self):
-		template = JINJA_ENVIRONMENT.get_template('html/questions.html')
+		template = JINJA_ENVIRONMENT.get_template('html/questions-chip.html')
 		self.response.write(template.render())
 
 class CompaniesHandler(webapp2.RequestHandler):
 	def get(self):
-		template = JINJA_ENVIRONMENT.get_template('html/companies.html')
+		template = JINJA_ENVIRONMENT.get_template('html/companies-chip.html')
 		self.response.write(template.render())
 
 class QuestionsFormHandler(webapp2.RequestHandler):
