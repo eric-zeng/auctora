3c7662a27477f567d88651d93e95d68fde9e5cec
dju90 <dju90@uw.edu>
2015-03-10 23:23:55 -0700

    minor css changes

--- js/canvas.js
+++ js/canvas.js
@@ -9,7 +9,7 @@ March 7, 2015
 (function() {						// anonymous function for window load event
 	window.addEventListener("load", function() {
 		$("#signature").jSignature()
-		document.getElementById("done").addEventListener("click", submitAnnotation);
+		$("#done").onclick(submitAnnotation);
 	});
 
 	// for submitting the annotation
