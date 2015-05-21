5743fddb613189b77a5939f78316d14281a512ed
Eric Zeng <ericzeng2@gmail.com>
2015-05-20 22:34:49 -0700

    Add navbar button functionality

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function scrollTo(div) {
+	document.getElementById(div).scrollIntoView(true);
+
+	// now account for fixed header
+	var scrolledY = window.scrollY;
+	if (scrolledY) {
+	  window.scroll(0, scrolledY - 50);
+	}
+}
